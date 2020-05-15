7b3d682c9e6e30a82d3bdf8983310f51d2d76961
Munib Emre Sevilgen <sevilgenemre@gmail.com>
2020-05-15 16:16:40 +0300

    update frontend for beckend

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,74 +1,51 @@
 import React, { Component } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './components/layouts/Navbar'
-import Footer from './components/layouts/Footer'
-
-import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
-
-
-class App extends Component{
-
-  render (){
-    return (
-      
-    <div>
-      <Navbar/>
-      <br/>
-       Excepteur anim consectetur deserunt excepteur eu culpa. Sunt sunt velit Lorem enim pariatur. Veniam esse nulla Lorem ut occaecat officia reprehenderit minim culpa irure et aliquip eu. Culpa commodo ex laboris laborum veniam sint. Amet dolor eu sint aliquip adipisicing aliquip exercitation fugiat magna cillum labore nisi ea nulla. 
-      <Footer/>
-    </div>
-    );
-
-  }
-}
-
-export default App;
-
-
-
-
-
-
-
-
-/*import React, { Component } from 'react';
 import axios from 'axios';
 
 class App extends Component {
-  state = {
-    email: '',
-    password: ''
-  }
-  
-  // componentDidMount() {
-  //   this.getAccounts();
 
-  // }
+  constructor(props){
+    super(props);
+    this.state = {email:"munib@emresevilgen.com", password:"sevilgen"};
+  }
+
 
   // getAccounts = _ => {
   //   axios.get('/login')
   //     .then((res) => {
-  //       console.log(res.data.accounts);
+  //       console.log(res.data);
   //       this.setState({ accounts: res.data.accounts });
   //     })
   //     .catch(error => console.log(error));
   // }
 
+  // componentDidMount() {
+  //   this.getAccounts();
+
+  // }
+
+
+
   //showAccounts = account => <div key={account.idAccount}> {account.name}</div>
   handleSubmit = event => {
     event.preventDefault();
-
-    var account = {
-      email: event.target.email.value,
-      password: event.target.password.value
-    };
-
-    axios.post('/login', { account })
+    var data = {
+      email: this.state.email,
+      password: this.state.password 
+    }
+    axios.post('/login', data)
       .then(res => {
         console.log(res);
-        console.log(res.data);
       })
+  } 
+
+  changeEmail = event => {
+    event.preventDefault();
+    this.setState({email: event.target.value});
+  }
+
+  changePassword = event => {
+    event.preventDefault();
+    this.setState({password: event.target.value});
   }
 
 
@@ -85,12 +62,12 @@ class App extends Component {
         <form onSubmit={this.handleSubmit}>
           <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <input value={this.state.email} onChange={this.changeEmail} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" />
+            <label onChange={this.changePassword} for="exampleInputPassword1">Password</label>
+            <input value={this.state.password} type="password" class="form-control" id="exampleInputPassword1" />
           </div>
           <div class="form-group form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1" />
@@ -104,4 +81,3 @@ class App extends Component {
 }
 
 export default App;
-*/
