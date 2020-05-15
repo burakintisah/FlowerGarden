8795f488e5c80e6b69a6498537676e2d8e6fc603
burakintisah <burak.intisah@gmail.com>
2020-05-15 19:29:42 +0300

    bugFixed

--- code/frontend/src/components/pages/ForgotPassword.js
+++ code/frontend/src/components/pages/ForgotPassword.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Form, FormGroup, Label, Input, Jumbotron } from 'reactstrap';
+import styled from 'styled-components'
 import Footer from '../layouts/Footer'
 
 class ForgotPassword extends Component {
@@ -8,27 +9,37 @@ class ForgotPassword extends Component {
             <div>
                 <Jumbotron>
                     <h1>FlowerGarden</h1>
-                    <h5> Forgot Password </h5>
-                    <div className="container">
-                        <Form >
-
+                    <h2> Welcome </h2>
+                    <LoginContainer>
+                        <Form className="login-form bk">
                             <FormGroup>
                                 <Label> Email</Label>
-                                <Input type="Email" placeholder="Email" />
+                                <Input type="email" placeholder="Email" />
                             </FormGroup>
                             <Button className="btn-lg btn-dark btn-block">Get New Password</Button>
-
-
                         </Form>
-
-                    </div>
-
+                    </LoginContainer>
                 </Jumbotron>
                 <Footer />
             </div>
+
 
         )
     }
 }
 
 export default ForgotPassword;
+
+
+
+const LoginContainer = styled.div`
+.login-form{
+    width: 100%;
+    max-width: 600px;
+    margin-left: 25px;
+    margin: auto;
+    height: 100%;
+    margin-top:8%;
+    margin-bottom:10%
+  }
+`;
--- code/frontend/src/components/pages/Login.js
+++ code/frontend/src/components/pages/Login.js
@@ -1,32 +1,37 @@
 import React, { Component } from 'react'
 import { Button, Form, FormGroup, Label, Input, Jumbotron } from 'reactstrap';
 import styled from 'styled-components'
+import Footer from '../layouts/Footer';
 
 export default class Login extends Component {
     render() {
         return (
-            <Jumbotron>
-                <h1>FlowerGarden</h1>
-                <h2> Welcome </h2>
-                <LoginContainer>
-                    <Form className="login-form bk">
-                        <FormGroup>
-                            <Label> Email</Label>
-                            <Input type="email" placeholder="Email" />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label> Password</Label>
-                            <Input type="password" placeholder="Password" />
-                        </FormGroup>
-                        <Button className="btn-lg btn-dark btn-block">Log in</Button>
-                        <div className='text-center mt-3 mb-3'>
-                            <a href="\sign-up">Sign up</a>
-                            <span className="p-2">|</span>
-                            <a href="\forgot-password">Forgot Password</a>
-                        </div>
-                    </Form>
-                </LoginContainer>
-            </Jumbotron>
+            <div>
+                <Jumbotron>
+                    <h1>FlowerGarden</h1>
+                    <h2> Welcome </h2>
+                    <LoginContainer>
+                        <Form className="login-form bk">
+                            <FormGroup>
+                                <Label> Email</Label>
+                                <Input type="email" placeholder="Email" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label> Password</Label>
+                                <Input type="password" placeholder="Password" />
+                            </FormGroup>
+                            <Button className="btn-lg btn-dark btn-block">Log in</Button>
+                            <div className='text-center mt-3 mb-3'>
+                                <a href="\sign-up">Sign up</a>
+                                <span className="p-2">|</span>
+                                <a href="\forgot-password">Forgot Password</a>
+                            </div>
+                        </Form>
+                    </LoginContainer>
+                </Jumbotron>
+
+                <Footer />
+            </div>
 
 
         )
