62b69a316088f584548fba1a8587582648d10851
Munib Emre Sevilgen <sevilgenemre@gmail.com>
2020-05-21 18:02:51 +0300

    update

--- code/backend/routes/arrangement.js
+++ code/backend/routes/arrangement.js
@@ -27,9 +27,10 @@ router.post('/customer/', (req, res) => {
     var val = [req.body.district_id];
 
     if (req.body.day && req.body.hour){
-      var occasion_query = " AND SWT.day=? AND SWT.hour=? ";
+      var day_query = " AND SWT.day=? AND SWT.hour=? ";
       val.push(req.body.day);
       val.push(req.body.hour);
+      query = query + day_query;
     }
 
     if (req.body.occasions && req.body.occasions.length > 0) {
@@ -58,6 +59,7 @@ router.post('/customer/', (req, res) => {
       val.push(req.body.price.upper);
       query = query + price_query;
     }
+    console.log(query + "--" + val);
   }
   dbconnection.query(query, val, function (err, result, fields) {
     if (err) {
